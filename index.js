8f63fd9dca995d2f1eade2c4e26c96cc46ba8c5e
Theau Poulat <theau@pop-os.localdomain>
2019-09-10 19:15:51 +0200

    add mongodb env variable

--- index.js
+++ index.js
@@ -9,9 +9,12 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-mongoose.connect("mongodb://localhost:27017/expenses-report", {
-  useNewUrlParser: true
-});
+mongoose.connect(
+	process.env.MONGODB_URI || "mongodb://localhost:27017/expenses-report",
+	{
+		useNewUrlParser: true
+	}
+);
 
 const Expense = models.expense;
 const User = models.user;
@@ -21,50 +24,50 @@ const User = models.user;
 // get all expenses
 
 app.get("/expense", async (req, res) => {
-  const response = await Expense.find({}).populate("user");
-  res.status(200).json(response);
+	const response = await Expense.find({}).populate("user");
+	res.status(200).json(response);
 });
 
 // post expenses and create user when it does not exist
 
 app.post("/expense", async (req, res) => {
-  const fetchIdOfUser = await User.findOne({ user: req.body.user });
-  console.log(fetchIdOfUser);
-  let idOfUser;
-  let response;
-  if (fetchIdOfUser === null) {
-    const newUser = new User({
-      user: req.body.user.toLowerCase(),
-      totalExpenses: req.body.amount
-    });
-    response = await newUser.save();
-    idOfUser = response._id;
-  } else {
-    idOfUser = fetchIdOfUser._id;
-    fetchIdOfUser.totalExpenses += Number(req.body.amount);
-    fetchIdOfUser.save();
-  }
+	const fetchIdOfUser = await User.findOne({ user: req.body.user });
+	console.log(fetchIdOfUser);
+	let idOfUser;
+	let response;
+	if (fetchIdOfUser === null) {
+		const newUser = new User({
+			user: req.body.user.toLowerCase(),
+			totalExpenses: req.body.amount
+		});
+		response = await newUser.save();
+		idOfUser = response._id;
+	} else {
+		idOfUser = fetchIdOfUser._id;
+		fetchIdOfUser.totalExpenses += Number(req.body.amount);
+		fetchIdOfUser.save();
+	}
 
-  const newExpense = new Expense({
-    user: idOfUser,
-    description: req.body.description,
-    amount: req.body.amount
-  });
-  await newExpense.save();
-  res.json({ status: 200, message: "Expense has been registered" });
+	const newExpense = new Expense({
+		user: idOfUser,
+		description: req.body.description,
+		amount: req.body.amount
+	});
+	await newExpense.save();
+	res.json({ status: 200, message: "Expense has been registered" });
 });
 
 // get all users
 
 app.get("/users", async (req, res) => {
-  const response = await User.find({});
-  res.status(200).json(response);
+	const response = await User.find({});
+	res.status(200).json(response);
 });
 
 app.all("*", (req, res) => {
-  res.send("all routes");
+	res.send("all routes");
 });
 
 app.listen(3001, () => {
-  console.log("Server Started");
+	console.log("Server Started");
 });
